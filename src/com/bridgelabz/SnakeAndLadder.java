5882a5a8c53436a646db313dd41c39c2fd2840f8
akashp69 <akashpakhare499@gmail.com>
2022-10-15 02:00:06 +0530

    Added:Program For  Repeat till Player reaches Position 100

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -4,22 +4,26 @@ import java.util.Scanner;
 
 public class SnakeAndLadder {
     public static void main(String[] args) {
-        int position=43;
+
+        int position=96;
         int snake= (int) Math.floor(Math.random() * 10) %3;
         int check =(int) Math.floor(Math.random() * 10) %6 +1;
         if(snake==1) {
             position =position + check;
-            System.out.println("Ladder Moves Ahead");
+            if(position<=100)
+            System.out.println("Ladder Option Moves Ahead " + position );
         }
         else if(snake==2)  {
             position =position - check;
-            System.out.println("Snake Moves Behind");
+            System.out.println("Snake Option Moves Behind " + position );
         }
         else {
             position=position;
             System.out.println("No Play");
-
         }
-        System.out.println("current postion after 43 is: " + position);
+        if(position<=100)
+            System.out.println("current postion after  is: " + position);
+        else
+            System.out.println("Throw Die Again");
 
-    }}
+        }}
