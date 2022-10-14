3183689f6ed2dbe0a0040bd48bd3cd3206a96282
akashp69 <akashpakhare499@gmail.com>
2022-10-15 00:49:28 +0530

    Added:Program For Player Roll die to get number between 1 to 6

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -3,11 +3,8 @@ package com.bridgelabz;
 public class SnakeAndLadder {
     public static void main(String[] args) {
 
-        System.out.println("Snake and Ladder game played with single player at start position 0");
-        int position=0;
-        int check =(int) (Math.random() * 10) %6 +1;
+        int check =(int) Math.floor(Math.random() * 10) %6 +1;
 
-        position=position + check;
-        System.out.println("Current position is :" + position);
+        System.out.println("Current position is :" + check);
     }
 }
