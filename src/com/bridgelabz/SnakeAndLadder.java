4d7582e8aa1666d95d63d289c3910cb8fcaad7da
akashp69 <akashpakhare499@gmail.com>
2022-10-15 01:16:50 +0530

    Added:Program For Player Checks A Option

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -1,10 +1,25 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class SnakeAndLadder {
     public static void main(String[] args) {
-
+        int position=43;
+        int snake= (int) Math.floor(Math.random() * 10) %3;
         int check =(int) Math.floor(Math.random() * 10) %6 +1;
+        if(snake==1) {
+            position =position + check;
+            System.out.println("Ladder Moves Ahead");
+        }
+        else if(snake==2)  {
+            position =position - check;
+            System.out.println("Snake Moves Behind");
+        }
+        else {
+            position=position;
+            System.out.println("No Play");
 
-        System.out.println("Current position is :" + check);
-    }
-}
+        }
+        System.out.println("current postion after 43 is: " + position);
+
+    }}
